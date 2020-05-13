6b210f1a2b40f5fe2b07b56063dce82bd20ee922
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2020-05-13 23:01:06 +0800

    update to swift 5

--- Example/Pods/Target Support Files/AutoCompleteTextField-iOS8.3/AutoCompleteTextField-iOS8.3-umbrella.h
+++ Example/Pods/Target Support Files/AutoCompleteTextField-iOS8.3/AutoCompleteTextField-iOS8.3-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_AutoCompleteTextField_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_AutoCompleteTextField_TestsVersionString[];
+FOUNDATION_EXPORT double AutoCompleteTextFieldVersionNumber;
+FOUNDATION_EXPORT const unsigned char AutoCompleteTextFieldVersionString[];
 
